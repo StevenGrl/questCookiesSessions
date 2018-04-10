d87ffd557f80ce28f741b483903c835c7e3916c7
StevenGrl <steven.grillon.45@gmail.com>
2018-04-10 12:42:39 +0200

    page login destroy session

--- login.php
+++ login.php
@@ -1,4 +1,8 @@
-<?php require 'inc/head.php'; ?>
+<?php
+session_start();
+session_destroy();
+require 'inc/head.php';
+?>
 <div class="container" style="margin-top:40px">
 <div class="row">
   <div class="col-sm-6 col-md-4 col-md-offset-4">
